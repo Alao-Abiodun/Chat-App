9fd4598ac994077d68349ddaa962cf194bcf244c
Alao Abiodun <abiodundev@gmail.com>
2025-01-01 07:38:43 +0100

    chore: setting up the form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,28 @@
 const SOCKET = io();
 
+const $MESSAGE_FORM = document.querySelector('#message-form');
+const $MESSAGE_FORM_INPUT = $MESSAGE_FORM.querySelector('input');
+const $MESSAGE_FORM_BUTTON = $MESSAGE_FORM.querySelector('button');
+const $LOCATION_BUTTON = document.querySelector('#send-location');
+
 SOCKET.on('message', (message) => {
     console.log(`${message}`);
 })
 
-document.querySelector('#message-form').addEventListener(
+$MESSAGE_FORM.addEventListener(
     'submit',
     (e) => {
         e.preventDefault();
 
+        $MESSAGE_FORM_BUTTON.setAttribute('disabled', 'disabled');
+
         let message = e.target.elements.message.value;
 
         SOCKET.emit('sendMessage', message, (error) => {
+            $MESSAGE_FORM_BUTTON.removeAttribute('disabled');
+            $MESSAGE_FORM_INPUT.value = '';
+            $MESSAGE_FORM_INPUT.focus();
+
             if (error) {
                 return console.log(error);
             }
@@ -20,18 +31,21 @@ document.querySelector('#message-form').addEventListener(
     }
 )
 
-document.querySelector('#send-location').addEventListener(
+$LOCATION_BUTTON.addEventListener(
     'click',
     () => {
         if (!navigator.geolocation) {
             return alert('Geolocation is not supported by your browser.')
         }
+
+        $LOCATION_BUTTON.setAttribute('disabled', 'disabled');
         
         navigator.geolocation.getCurrentPosition((position) => {
             SOCKET.emit('sendLocation', {
                 latitude: position.coords.latitude, 
                 longitude: position.coords.longitude
             }, () => {
+                $LOCATION_BUTTON.removeAttribute('disabled');
                 console.log('Location shared!')
             });
         })
